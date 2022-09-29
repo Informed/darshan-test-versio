3b28c029bc3c522842df746910d136cddf7edec4
darshan2403 <darshan.shah@informed.iq>
2022-09-29 14:49:30 -0700

    update script

--- auto_tag.sh
+++ auto_tag.sh
@@ -37,18 +37,10 @@ NEW_TAG="$APPNAME-$VNUM1.$VNUM2.$VNUM3"
 
 echo "INFO: Updating $VERSION to $NEW_TAG"
 
-#get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
-
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
-if [ -z "$NEEDS_TAG" ]; then
-    echo "INFO: Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-    git tag $NEW_TAG 
-    git push --tags 1> /dev/null
-else
-    echo "WARN: Already a tag on this commit"
-fi
+echo "INFO: Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+git tag $NEW_TAG 
+git push --tags 1> /dev/null
 
 ## If its prod commit 
 ## Get the 
