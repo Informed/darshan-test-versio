99fa88a9e9f7d2a14de9d28cf5eed850e7bbe6d3
darshan2403 <darshan.shah@informed.iq>
2022-10-10 16:38:02 -0700

    aws

--- infrastructure/partner_profile/sand/dshah/variables.tf
+++ infrastructure/partner_profile/sand/dshah/variables.tf
@@ -10,7 +10,7 @@ variable "environment" {
 }
 
 variable "partner_profile_honeybadger_api_key" {
-  description = "API Key for Partnesadr Profile Honeybadger accesaasdsdjkkjhhs"
+  description = "API Key for Partnesadr Profile Honeybadger accesaaasdsdsdjkkjhhs"
   type        = string
   default     = "hbp_PRwWENiasCPczk1r70XXFQ3dJEQCN52K6FWc"
 }
