5020ebf787b3d3907048e382283f9060742372ef
darshan2403 <darshan.shah@informed.iq>
2022-10-18 14:13:35 -0700

    Trigger

--- infrastructure/api_handler/sand/dshah/variables.tf
+++ infrastructure/api_handler/sand/dshah/variables.tf
@@ -104,7 +104,7 @@ variable "lambda_tracing_mode" {
 }
 
 variable "extra_environment_variables" {
-  description = "Extra environment variables to seaasdsdaaasdsdstaaasasasdasdddsdasdasaasaZZXsasdadddasdasasddssaasdssdaasasdadasdeasasdsasdaaesdasdaasdssddad"
+  description = "Extra environment variables to seaasdsasdadaaasdsdstaaasasasdasdddsdasdasaasaZZXsasdadddasdasasddssaasdssdaasasdadasdeasasdsasdaaesdasdaasdssddad"
   type        = map(string)
   default     = {}
 }
