58beb0173ab72e14997582cfdbe1ba9431dc8517
darshan2403 <darshan.shah@informed.iq>
2022-10-19 10:39:08 -0700

    update tf

--- infrastructure/api_handler/sand/dshah/variables.tf
+++ infrastructure/api_handler/sand/dshah/variables.tf
@@ -104,7 +104,7 @@ variable "lambda_tracing_mode" {
 }
 
 variable "extra_environment_variables" {
-  description = "Extra environment variables to seaasdsasdadaaasasddsdstaaaasdsasasdasdddsdasdasaasaZZXsasdadddasdasasddssaasdssdaasasdadasdeasasdsasdaaesdasdaasdssddad"
+  description = "Extra environment variables to seaasdsasdadaaasasddsdstasadaaasdsasasdasdddsdasdasaasaZZXsasdadddasdasasddssaasdssdaasasdadasdeasasdsasdaaesdasdaasdssddad"
   type        = map(string)
   default     = {}
 }
