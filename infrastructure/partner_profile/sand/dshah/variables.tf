a480f25e173b2f26f07c2c8dabf5db92e8eb5d17
darshan2403 <darshan.shah@informed.iq>
2022-10-19 15:02:31 -0700

    update partner_profile

--- infrastructure/partner_profile/sand/dshah/variables.tf
+++ infrastructure/partner_profile/sand/dshah/variables.tf
@@ -92,7 +92,7 @@ variable "lambda_tracing_mode" {
 }
 
 variable "extra_environment_variables" {
-  description = "Extra environasdasdment variables to set"
+  description = "Extra environasdasdmasdasdent variables to set"
   type        = map(string)
   default     = {}
 }
