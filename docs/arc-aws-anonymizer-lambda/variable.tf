cb46c716a9e687d1ec0b97a7940077152fe609c7
darshan2403 <darshan.shah@informed.iq>
2022-11-08 23:52:27 -0800

    remove profile and env

--- docs/arc-aws-anonymizer-lambda/variable.tf
+++ docs/arc-aws-anonymizer-lambda/variable.tf
@@ -1,8 +1,3 @@
-variable "environment" {
-  description = "Name of this environment"
-  type        = string
-}
-
 variable "region" {
   description = "Region to deploy terraform resources to"
   type        = string
@@ -11,11 +6,6 @@ variable "region" {
 
 variable "app_name" {
   description = "Application name"
-  type        = string
-}
-
-variable "profile" {
-  description = "AWS profile to use"
   type        = string
 }
 
