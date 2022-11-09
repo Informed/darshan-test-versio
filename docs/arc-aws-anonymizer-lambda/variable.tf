fb3a27bd1ea96e6f48f59b47eb090808fe344a37
darshan2403 <darshan.shah@informed.iq>
2022-11-08 23:59:53 -0800

    Update env

--- docs/arc-aws-anonymizer-lambda/variable.tf
+++ docs/arc-aws-anonymizer-lambda/variable.tf
@@ -1,3 +1,8 @@
+variable "environment" {
+  description = "Name of this environment"
+  type        = string
+}
+
 variable "region" {
   description = "Region to deploy terraform resources to"
   type        = string
