84f6db2f07dfd099fb9f5a875742a7b486f064af
darshan2403 <darshan.shah@informed.iq>
2022-11-10 11:26:18 -0800

    update

--- docs/arc-aws-anonymizer-lambda/variable.tf
+++ docs/arc-aws-anonymizer-lambda/variable.tf
@@ -30,12 +30,12 @@ variable "dest_bucket" {
 
 variable "add_documents_PII" {
   type    = string
-  default = "'first_name','last_name','middle_name','suffix','email','ssn','driver_license_number','date_of_birth','dob','account_number','bank_account_number','vin','id_number', 'policy_number','tin','itin','applicant_phone_number','trade_in_vin','zip','city','state','street_2','street_address','phone'"
+  default = "first_name,last_name,middle_name,suffix,email,ssn,driver_license_number,date_of_birth,dob,account_number,bank_account_number,vin,id_number, policy_number,tin,itin,applicant_phone_number,trade_in_vin,zip,city,state,street_2,street_address,phone"
 }
 
 variable "remove_documents_PII" {
   type    = string
-  default = "'analysis_document_payload'"
+  default = "analysis_document_payload"
 }
 
 ###
@@ -44,12 +44,12 @@ variable "remove_documents_PII" {
 
 variable "add_application_PII" {
   type    = string
-  default = "'first_name','last_name','middle_name','suffix','email','ssn','date_of_birth','dob','account_number','bank_account_number','vin','id_number', 'policy_number','tin','itin','zip','city','state','street_2','street_address','phone'"
+  default = "first_name,last_name,middle_name,suffix,email,ssn,date_of_birth,dob,account_number,bank_account_number,vin,id_number, policy_number,tin,itin,zip,city,state,street_2,street_address,phone"
 }
 
 variable "remove_application_PII" {
   type    = string
-  default = "''"
+  default = ""
 }
 
 ###
@@ -58,17 +58,17 @@ variable "remove_application_PII" {
 
 variable "add_stip_verification_PII" {
   type    = string
-  default = "'matches_applicant_name', 'matches_applicant2_name','matches_applicant1_name','matches_applicant1_ssn', 'matches_applicant1_address', 'matches_applicant2_ssn', 'matches_applicant2_address', 'matches_applicant1_dob', 'matches_applicant2_dob','matches_applicant1_vin', 'matches_applicant2_vin', 'matches_applicant_address','matches_applicant_ssn','vin','policy_number','matches_applicant_vin', 'matches_approval_vin','account_number','matches_contract_vin','matches_approval_vin','dob','is_ssi_deposit_referencing_applicant_name','matches_applicant_dob'"
+  default = "matches_applicant_name, matches_applicant2_name,matches_applicant1_name,matches_applicant1_ssn, matches_applicant1_address, matches_applicant2_ssn, matches_applicant2_address, matches_applicant1_dob, matches_applicant2_dob,matches_applicant1_vin, matches_applicant2_vin, matches_applicant_address,matches_applicant_ssn,vin,policy_number,matches_applicant_vin, matches_approval_vin,account_number,matches_contract_vin,matches_approval_vin,dob,is_ssi_deposit_referencing_applicant_name,matches_applicant_dob"
     
 }
 
 variable "add_stip_verification_list_PII" {
   type    = string
-  default = "'recommendations'"
+  default = "recommendations"
     
 }
 
 variable "remove_stip_verification_PII" {
   type    = string
-  default = "'analysis_document_payload'"
+  default = "analysis_document_payload"
 }
