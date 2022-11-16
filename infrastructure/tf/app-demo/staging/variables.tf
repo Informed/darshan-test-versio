caedfc0271b2f3fb860b6919fdf4df6352d2f44e
darshan2403 <darshan.shah@informed.iq>
2022-11-15 22:09:29 -0800

    trigger

--- infrastructure/tf/app-demo/staging/variables.tf
+++ infrastructure/tf/app-demo/staging/variables.tf
@@ -104,7 +104,7 @@ variable "lambda_tracing_mode" {
 }
 
 variable "extra_environment_variables" {
-  description = "Extra environasdment variables to set"
+  description = "Extra environasdmeasdnt variables to set"
   type        = map(string)
   default     = {}
 }
