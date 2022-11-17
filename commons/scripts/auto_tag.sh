dc07b76cc891cfe5db912521bfd69fadd07a9fde
darshan2403 <darshan.shah@informed.iq>
2022-11-17 15:20:52 -0800

    udpate

--- commons/scripts/auto_tag.sh
+++ commons/scripts/auto_tag.sh
@@ -69,8 +69,8 @@ newTag="${appName}-${majorVersion}.${minorVersion}.${patchVersion}-${preRelease}
 
 echo "export GIT_TAG=${newTag}" >> $BASH_ENV
 ### To make it usable across workspace ###
-mkdir -p /tmp/workspace
-echo "export GIT_TAG=\"${GIT_TAG}\"" >> /tmp/workspace/env_vars
+mkdir -p tmp_workspace
+echo "export GIT_TAG=\"${GIT_TAG}\"" >> tmp_workspace/env_vars
 
 if [[ "${CIRCLE_PULL_REQUEST##*/}" = "" ]]; then
     echo "Pull Request Merge to main branch event..."
