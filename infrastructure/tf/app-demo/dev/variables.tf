148a7053822dd9b6ee240540f974fc15b3c28177
darshan2403 <darshan.shah@informed.iq>
2022-11-17 16:12:31 -0800

    Update

--- infrastructure/tf/app-demo/dev/variables.tf
+++ infrastructure/tf/app-demo/dev/variables.tf
@@ -16,7 +16,7 @@ variable "environment" {
 }
 
 variable "app_demo_honeybadger_api_key" {
-  description = "API Key for API Handler Honeybaasdsdadger accasdasaszdesasds"
+  description = "API Key for API Handler Honeyasdbaasdsdadger accasdasaszdesasds"
   type        = string
   default     = "hbp_2xj5epE6rHbEgxxkw95dDSQ5O4iESx3F6GDE"
 }
