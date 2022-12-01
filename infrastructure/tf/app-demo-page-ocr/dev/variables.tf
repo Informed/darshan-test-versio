77edb28bab93693e4186798cd1a8705b3a6b29f1
Darshan Shah <111308651+darshans2403@users.noreply.github.com>
2022-12-01 13:27:12 -0800

    trigger tf dev app demo page ocr

--- infrastructure/tf/app-demo-page-ocr/dev/variables.tf
+++ infrastructure/tf/app-demo-page-ocr/dev/variables.tf
@@ -17,7 +17,7 @@ variable "bucket_prefix" {
 }
 
 variable "project_name" {
-  description = "Name of Project"
+  description = "Name of Proasdject"
   type        = string
   default     = "techno"
 }
